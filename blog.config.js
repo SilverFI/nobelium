b67af8b5f6080a64bd9c9b076955be4fe6b82339
SilverFI <SilverFI@users.noreply.github.com>
2023-04-01 14:58:36 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'SilverFI',
   email: '',
   link: 'https://silverfi.vercel.app/',
-  description: 'Daily log of SilverFI',
+  description: 'daily log of work and learning',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
